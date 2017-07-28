fee973da09e2aa875658126db14b7996fe13431f
xafero <georg.berendt@uni-oldenburg.de>
2017-07-29 01:22:08 +0200

    Working on RPM support

--- etc/build.bat
+++ etc/build.bat
@@ -1,1 +1,1 @@
-c:\ikvm\bin\ikvmc -target:library commons-compress-1.14.jar dec-0.1.2.jar xz-1.6.jar
+c:\ikvm\bin\ikvmc -target:library redline-1.2.5.jar commons-compress-1.14.jar xz-1.6.jar dec-0.1.2.jar slf4j-api-1.7.25.jar slf4j-simple-1.7.25.jar bcpg-jdk15on-1.50.jar bcprov-jdk15on-1.50.jar
