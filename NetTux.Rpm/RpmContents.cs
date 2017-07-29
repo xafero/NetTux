3ebcec2c3b406c08473947ee5d42fcdca4ba76b0
xafero <georg.berendt@uni-oldenburg.de>
2017-07-29 15:53:34 +0200

    Fixed root and documentation flags

--- NetTux.Rpm/RpmContents.cs
+++ NetTux.Rpm/RpmContents.cs
@@ -20,9 +20,11 @@ namespace NetTux.Rpm
             var source = new File(c.Source);
             var directive = Directive.NONE;
             const bool addParents = true;
-            var path = FixSlash(c.Path);
+            var path = FixSlash(c.Path).TrimStart('.');
             var permissions = c.Permissions ?? GetPermissions(path, true);
             var dirMode = GetPermissions(path, false);
+            if (path.Contains("/doc/"))
+                directive = Directive.DOC;
             _includes.addFile(path, source, permissions, directive, c.User, c.Group, dirMode, addParents);
         }
     }
